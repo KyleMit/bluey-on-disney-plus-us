2354897bbc9aa09307c453e51cd49112436ecea4
KyleMit <kmitofsky@stackoverflow.com>
2026-02-14 14:01:45 -0500

    perf round

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,20 @@
+const htmlmin = require("html-minifier-terser");
+
 module.exports = function(eleventyConfig) {
+  // Minify HTML output in production
+  eleventyConfig.addTransform("htmlmin", function(content) {
+    if (this.page.outputPath && this.page.outputPath.endsWith(".html")) {
+      return htmlmin.minify(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true,
+        minifyCSS: true,
+        minifyJS: true
+      });
+    }
+    return content;
+  });
+
   return {
     dir: {
       input: "src",
